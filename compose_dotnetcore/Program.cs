17972f82b0fb89cf3972eeb03fc41e9640a7d2d1
Jeroen <gitlab.com@hendricksen.eu>
2021-12-15 10:56:00 +0100

    Add many compose examples

---
+++ compose_dotnetcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace com.infosupport.dockeress.example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ compose_golang/README.md
@@ -0,0 +1,3 @@
+# Example
+
+An example to demonstrate the effects of docker build caching.
