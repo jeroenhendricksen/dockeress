c64d8a00909deea3c28d20d932b8c6a6c0008f7f
Jeroen Hendricksen <gitlab.com@hendricksen.eu>
2022-08-31 22:42:01 +0200

    Add springboot example

---
+++ compose_springboot/src/main/java/com/example/javademo/DemoApplication.java
@@ -0,0 +1,40 @@
+package com.example.javademo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PreDestroy;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+}
--- compose_springboot/src/main/java/com/example/javademo/HelloController.java
+++ compose_springboot/src/main/java/com/example/javademo/HelloController.java
@@ -1,2 +1,13 @@
-package com.example.javademo;public class HelloController {
+package com.example.javademo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 }
