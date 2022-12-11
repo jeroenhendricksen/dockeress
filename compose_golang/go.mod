a15a755e3550c9f960bfd52d0c6e68d1b7f6cfb6
Jeroen Hendricksen <gitlab.com@hendricksen.eu>
2022-12-11 21:29:53 +0100

    Improve arm compatibility

--- compose_golang/go.mod
+++ compose_golang/go.mod
@@ -1,3 +1,3 @@
 module learn
 
-go 1.17
+go 1.19
--- compose_nodejs/server.js
+++ compose_nodejs/server.js
@@ -1,15 +1,21 @@
 'use strict';
 
+// Imports
 const express = require('express');
+const os = require("os");
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+// get host name
+const hostName = os.hostname();
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World from NodeJS!');
+  res.setHeader('content-type', 'text/html');
+  res.send(`<h2>Hello World from hostname <code>${hostName}</code>(using NodeJS)</h2>`);
 });
 
 app.listen(PORT, HOST);
