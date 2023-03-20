48f5150916751275ce5a8f5aeca1943eeebbc3d9
Jeroen Hendricksen <gitlab.com@hendricksen.eu>
2023-03-20 14:55:29 +0100

    Update springboot example

--- compose_springboot/src/main/java/com/example/javademo/DemoApplication.java
+++ compose_springboot/src/main/java/com/example/javademo/DemoApplication.java
@@ -2,17 +2,13 @@ package com.example.javademo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import javax.annotation.PreDestroy;
 import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -27,7 +23,7 @@ public class DemoApplication {
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			LOGGER.info("Let's inspect the beans provided by Spring Boot:");
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
